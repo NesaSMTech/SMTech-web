51937c184224151133fa60b34ad03eb37e1935a8
NesaSMTech <smtech1991@gmail.com>
2024-08-02 15:01:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function setLanguage(lang) {
+    document.querySelectorAll('[data-en]').forEach(el => {
+        el.innerText = el.getAttribute(`data-${lang}`);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setLanguage('en'); // Set default language to English
+});
